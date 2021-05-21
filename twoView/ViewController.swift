07c7b8c3b34a791a2f55361988b0b566140a1712
nicholas11858 <nicholas11858@gmail.com>
2021-05-21 13:35:01 +0200

    Initial Commit

---
+++ twoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twoView
+//
+//  Created by user192060 on 5/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
